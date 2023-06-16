b89ba7c44981a728948fe321b763e083f40ee617
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-15 21:33:57 -0700

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - compiles data type size,
- * 
+ * byte sizse
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -13,6 +11,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-
 	return (0);
 }
