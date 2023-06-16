a8823ba0d5e4b3c852cfa2e6dbdb1faa6eabebec
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-15 21:32:13 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - print texts on the display, 
+ * main - print texts on the display,
+ * but the outcome
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
