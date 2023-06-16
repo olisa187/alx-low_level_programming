70525b24e8d9f73104fbf74ff67e5f5e9d632045
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-15 21:02:32 -0700

    corrected betty code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/* c program to print to tjhe diplay using puts function */
+/**
+ * c program to print to the diplay using puts function 
+ */
 
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle\n");
