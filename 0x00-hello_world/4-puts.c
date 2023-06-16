1fefc7c477cf1dccb1ef7a4b2749950e3dde02e2
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-15 21:36:06 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - print texts on the display,
- *
+ * programming is like....
  * Return: Always 0 (Success)
  */
-
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle\n");
-
 	return 0;
 }
