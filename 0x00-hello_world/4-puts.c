7b89dc3bcf2f9cf27f997b414d5420969cc8ec41
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-15 20:19:09 -0700

    c program to print using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* c program to print to tjhe diplay using puts function */
+
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return 0;
+}
