d8e48263875e380c6690feff107f0e646ee4c6ea
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-15 21:27:26 -0700

    remove white spaces anf encosed return value in brackets

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print texts on the display,
- * 
+ * main - print texts on the display, 
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return 0;
+	return (0);
 }
