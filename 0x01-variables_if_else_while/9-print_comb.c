4edc95d966e31649eb75abc0ec58cf2ff5afaeb0
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-16 21:37:24 -0700

    separator

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits
+ * digits are separated by commas and space
+ * Return: on success 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num == 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
