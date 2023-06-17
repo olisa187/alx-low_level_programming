3cdb970441207ba16e8c2daf43b4cfd214632c44
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-16 19:11:52 -0700

    betty correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - function that deals with logical statement
  * The code to check if a numbe is either positive negative or zero
@@ -9,6 +10,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
