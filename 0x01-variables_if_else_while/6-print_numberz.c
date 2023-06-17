46936794c4f350f88b51e8a099c5e04f535ac845
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-16 20:42:46 -0700

    translating putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - single digits
+ * display 
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
