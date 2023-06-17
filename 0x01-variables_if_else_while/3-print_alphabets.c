6cf2643909d372e0ab8bd1b792f22e073545246d
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-16 20:16:38 -0700

    upper and lower cas letters combined

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - display lowercase and uppercase letter
+ * uses putchar function to acheieve this task
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+	/* create a loop */
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar("\n");
+	return (0);
+}
