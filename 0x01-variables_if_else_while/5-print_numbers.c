7beec25201a07fdfc1e76892de3097280e70e70f
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-16 20:34:19 -0700

    Number display from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - loop
+ * display single digits
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
