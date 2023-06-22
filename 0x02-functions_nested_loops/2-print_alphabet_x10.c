a126107307c10a0a92a18a2dc15d33763f67a0aa
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-21 19:19:39 -0700

    nested looping in a function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints letter a to z completed on 10 different lines
+ * program will be acheived using a loop
+ * program ends with a new line
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z', alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
