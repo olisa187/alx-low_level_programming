22fcb2560eacb946dcaeaf146282cf6fbecae1d8
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-21 21:11:01 -0700

    correction done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,7 +6,7 @@
 void times_table(void)
 {
 	int i;
-       	int j;
+	int j;
 	int k;
 
 	for (i = 0; i < 10; i++)
@@ -24,7 +24,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			} 
+			}
 			else if (k >= 10)
 			{
 				_putchar(',');
