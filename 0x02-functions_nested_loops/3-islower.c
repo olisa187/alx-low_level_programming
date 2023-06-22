796f13e05a540755f468106676136c43f30c0403
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-21 19:34:56 -0700

    checks an alphabet is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks which alphabet has a lower ascii value
+ * the program either return 0 or 1 depending on the boolean condition returned
+ * Return: 0 on success
+ */
+int _islower(int c)
+{
+	int ans = c >= 'a' && c <= 'z';
+	return (ans);
+}
