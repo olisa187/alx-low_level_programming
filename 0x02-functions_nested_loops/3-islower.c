1bf3ff7bc64d16e9f2ceecdcbc7fad2d54ec3fdf
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-21 19:37:40 -0700

    code updated

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,10 +3,12 @@
 /**
  * _islower - checks which alphabet has a lower ascii value
  * the program either return 0 or 1 depending on the boolean condition returned
+ * @c - function parameter
  * Return: 0 on success
  */
 int _islower(int c)
 {
 	int ans = c >= 'a' && c <= 'z';
+
 	return (ans);
 }
