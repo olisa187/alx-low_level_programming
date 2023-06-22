5267e65059dbc74cf873dd668404309eea4fe68b
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-21 20:13:05 -0700

    updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_sign - takes an argument to check wheather it is less, 
+ * print_sign - takes an argument to check wheather it is less,
  * greater than or equal to 0
  * @n: parameter to be checked in the function
- * Return: 0 if the parameter is equal to 0 1 
- * if the parameter id greater than 0 and -1 
+ * Return: 0 if the parameter is equal to 0 1
+ * if the parameter id greater than 0 and -1
  * if it is less than 0
  */
 int print_sign(int n)
