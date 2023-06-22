e1735af4432d173b083f026599a94949e0e58b29
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-21 19:09:09 -0700

    Changes made to the betty code style of c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - program that print a to z
+ * print_alphabet - program that print a to z
  * with the use if for loop
  * Return: on success return 0
  */
 void print_alphabet(void)
 {
 	char alpha;
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
