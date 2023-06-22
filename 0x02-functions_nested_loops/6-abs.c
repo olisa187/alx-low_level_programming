e48911f925a198ee4c5db90b17e0f299ccf0709d
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-21 20:18:40 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - function to check the absoulte value
+ * @n: parameter to check
+ * Return: 0 on success
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
