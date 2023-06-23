608e4151f7921b6e1accbc900d8f3290b9a94a75
olisaemeka <victorolisaemekaaghabuilo@yahoo.com>
2023-06-22 19:15:54 -0700

    function that check is digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks that a given character is a digits
+ * @c: parameter given in the function
+ * Return: excutes on sucess
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (c);
+	}
+}
